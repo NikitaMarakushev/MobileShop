5295b2d6eeb44bf3ba0b7d1678f800741a194648
NikitaMarakushev <nikitamarakushev@yandex.ru>
2021-02-05 00:57:35 +0300

    initial commit: basic ground

---
+++ mobileshop/mobileshop1/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import HeaderComponent from './usingcomponents/HeaderComponent';
+import FooterComponent from './usingcomponents/FooterComponent';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <HeaderComponent />
+    <App />
+    <FooterComponent />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+reportWebVitals();
---
+++ mobileshop/mobileshop1/src/usingcomponents/FooterComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FooterComponent() {
+    return (
+        <div>
+            <h2>Footer</h2>
+        </div>
+    )
+}
