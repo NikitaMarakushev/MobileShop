59ae872817a98343d3b27ea601fcdd61b9b65480
NikitaMarakushev <nikitamarakushev@yandex.ru>
2021-02-07 02:16:33 +0300

    header created

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/scss/main.scss';
-import { HeaderComponent, FooterComponent } from './usingcomponents';
-
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HeaderComponent />
     <App />
-    <FooterComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/usingcomponents/HeaderComponent.jsx
+++ src/usingcomponents/HeaderComponent.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
+import headerLogo from '../media/header_logo.svg';
 
 function HeaderComponent() {
     return (
         <div className="container bg-info header">
-            <div className="row">
-                <div className="col-lg-2">Itme1</div>
-                <div className="col-lg-2">Itme2</div>
-                <div className="col-lg-2">Itme3</div>
+            <div className="d-flex row">
+                <div className="d-flex col-2 flex-row w-100 justify-content-between p-2">
+                    <div className="d-flex col-lg-15 align-items-center">
+                        <a href="" className="text-light">Itme1</a>
+                    </div>
+                    <div className="d-flex col-lg-15 align-items-center">
+                        <a href="" className="text-light"> Itme2</a>
+                    </div>
+                    <div className="d-flex col-lg-15 align-items-center">
+                        <a href="" className="text-light">Itme2</a>
+                    </div>
+                </div>
+                <div className="ml-auto p-2 flex-end">
+                    <img className="header_img "src={headerLogo} alt="bitten logotype"/>
+                </div>
             </div>
         </div>
     )
